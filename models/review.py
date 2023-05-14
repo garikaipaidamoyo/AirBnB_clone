b1fe67deef23d07a5005c186411c798256ab78c5
paidamoyo <garikaipaidamoyo@gmail.com>
2023-05-14 01:53:16 -0700

    other model files

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creates Review class"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Class for managing review objects"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
