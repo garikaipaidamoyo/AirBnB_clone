64b8ece989264c85b93385f3f3d8ddab5516b511
paidamoyo <garikaipaidamoyo@gmail.com>
2023-05-14 01:09:54 -0700

    Adding models file

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that creates Amenity class"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Class for managing amenity objects"""
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""" Creates User class"""
+
+from models.base_model import BaseModel
+
+class city(BaseModel):
+    """Class for managing city objects"""
+    state_id = ""
+    name = ""
