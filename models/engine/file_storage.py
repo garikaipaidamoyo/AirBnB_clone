47c01eb567c23b7e8a353ec4341acad72e65e302
NyatiZw <appsmaid19@gmail.com>
2023-05-14 17:58:28 -0700

    checking pycodestyle on files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,37 +59,37 @@ class FileStorage:
     def attributes(self):
         """Returns the attributes for classname"""
         attributes = {
-                "BaseModel":
-                         {"id": str,
-                          "created_at": datetime.datetime,
-                          "updated_at": datetime.datetime},
-                "User":
-                         {"email": str,
-                          "password": str,
-                          "first:name": str,
-                          "last_name": str},
-                "State":
-                         {"name": str},
-                "City":
-                         {"state_id": str,
-                          "name": str},
-                "Amenity":
-                         {"name": str},
-                "Place":
-                         {"city_id": str,
-                          "user_id": str,
-                          "name": str,
-                          "description": str,
-                          "number_rooms": int,
-                          "number_bathrooms": int,
-                          "max_guest": int,
-                          "price_by_night": int,
-                          "latitude": float,
-                          "longitude": float,
-                          "amenity_ids": list},
-                "Review":
-                         {"place_id": str,
-                          "user_id": str,
-                          "text": str}
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first:name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+                     {"place_id": str,
+                      "user_id": str,
+                      "text": str}
             }
         return attributes
